c2266ac079059ce6c671b8f968f6b52f2645fcc6
Christopher <chris@madmullins.com>
2017-01-30 07:13:23 -0600

    Second push, slight changes and cleanup.

--- YouTubeR/YouTubeR/My Project/Resources.Designer.vb
+++ YouTubeR/YouTubeR/My Project/Resources.Designer.vb
@@ -59,15 +59,5 @@ Namespace My.Resources
                 resourceCulture = value
             End Set
         End Property
-        
-        '''<summary>
-        '''  Looks up a localized resource of type System.Byte[].
-        '''</summary>
-        Friend ReadOnly Property youtube_dl() As Byte()
-            Get
-                Dim obj As Object = ResourceManager.GetObject("youtube_dl", resourceCulture)
-                Return CType(obj,Byte())
-            End Get
-        End Property
     End Module
 End Namespace
